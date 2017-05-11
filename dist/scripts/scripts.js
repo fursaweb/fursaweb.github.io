d69c5ed7424be12a88e9d0c80aa8fbae9dc5f21f
Олег Фурса <oleg.fursa@webstudio.biz>
2017-05-11 21:55:23 +0300

    правки

--- dist/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    $('.menu-btn').click( function () {
-       $(this).toggleClass('active');
-       $('.side-menu').toggleClass('is-visible');
-    });
-    console.log('ku');
-});
